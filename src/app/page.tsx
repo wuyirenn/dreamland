d84b3033505ba1dfa572aabd18a6830a35eed6de
Christian Wu <wuyirenn@gmail.com>
2024-10-25 18:22:22 -0700

    fix attempt #1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
         audioRef.current = null;
       }
     };
-  }, []);
+  });
 
   useEffect(() => {
     const playAudio = async () => {
--- src/components/cards/about-me.tsx
+++ src/components/cards/about-me.tsx
@@ -25,21 +25,21 @@ const AboutMe = () => {
                 </div>
                 <br></br>
                 <div className="font-semibold text-6xl lg:text-7xl text-left">
-                    I'm a people-oriented <br />
+                    {`I'm a people-oriented `}<br />
                     <Roles />
                     focused on environmental action and social equity.
                 </div>
                 <br></br>
                 <div className="grid md:grid-cols-5 xl:grid-cols-9 gap-4 text-xs text-left font-medium ml-2">
                     <div className="md:col-span-3 xl:col-span-4">
-                        I fell in love with nature in the era of digital point-and-shoots. Through the tiny, electric viewfinder of my mom's 
+                        {`I fell in love with nature in the era of digital point-and-shoots. Through the tiny, electric viewfinder of my mom's 
                         silver Canon, I'd capture incredible wonders -- glorious views at national parks, but also daily sunsets, neighbourhood 
                         butterflies, and garden roses. I've learned that there is always magic to be found, if only I look. And I strive to convey 
-                        this beauty through my art and photography today.
+                        this beauty through my art and photography today.`}
                     </div>
                     <div className="md:col-span-2 xl:col-span-3">
-                        I work on projects that make our built environment more sustainable and better designed. 
-                        I'm an explorer at heart, driven by learning and creating new things, and I use my versatility and communication to connect people, align action, and get sh*t done.
+                        {`I work on projects that make our built environment more sustainable and better designed. 
+                        I'm an explorer at heart, driven by learning and creating new things, and I use my versatility and communication to connect people, align action, and get sh*t done.`}
                     </div>
                     <div className="col-span-full xl:col-span-2">
                         ON REPEAT
