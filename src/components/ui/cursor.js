b8054f2adc893e94c6743f4240b7681093a0e21c
Christian Wu <wuyirenn@gmail.com>
2025-03-16 01:51:40 -0700

    Reduce cursor speed

--- src/components/ui/cursor.js
+++ src/components/ui/cursor.js
@@ -26,8 +26,8 @@ const Cursor = ({ isActive, radius }) => {
         
         if (Math.abs(dx) > 0.01 || Math.abs(dy) > 0.01) {
             delayedMouse.current = {
-                x: lerp(delayedMouse.current.x, mouse.current.x, 0.2),
-                y: lerp(delayedMouse.current.y, mouse.current.y, 0.2)
+                x: lerp(delayedMouse.current.x, mouse.current.x, 0.1),
+                y: lerp(delayedMouse.current.y, mouse.current.y, 0.1)
             };
             
             if (circle.current) {
